3e5af961312120e7cb0b2c9825470085cd99ea98
Victor Gustavo Reyes Vidal <prism@Arabella.local>
2026-05-22 08:33:56 -0600

    fix: inyeccion de credenciales de firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAu19dBMOe8P07M8K7Yesr10lh3RmwI4zk",
+    authDomain: "lifegoals-core.firebaseapp.com",
+    projectId: "lifegoals-core",
+    storageBucket: "lifegoals-core.firebasestorage.app",
+    messagingSenderId: "806951559423",
+    appId: "1:806951559423:web:1546a3e1b9102fa7f3acca",
+    measurementId: "G-TVPPVTG934"
+  }
+};
